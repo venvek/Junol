8d0dce70050e835704e94e48a05ec476413462dd
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-27 16:42:04 +0900

    c

---
+++ Java2024/src/d0327/C3.java
@@ -0,0 +1,20 @@
+package d0327;
+
+public class C3 {
+	int plus(int x, int y, int z) {
+		int result = x + y;
+		return result;
+		int result3 = 2(x * y)+ 2(x * z)+2(y * z)
+	}
+	
+	double plus(double x, double y) {
+		double result = x + y;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		int res1 = myCalc.plus(5, 10);
+	}
+
+}
---
+++ Java2024/src/d0327/Static93.java
@@ -0,0 +1,13 @@
+package d0327;
+
+public class Static93 {
+	static double pi = 3.14159
+			
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	static int minus(int x, int y) {
+		return x - y;
+	}
+	
+	
