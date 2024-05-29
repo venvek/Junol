2e02c6c68962dbdbd2e16a9b24d38395bf4e4c6b
21-manic-016g <21-manic-016g@21-manic-016>
2024-05-29 17:44:30 +0900

    0529 진성

--- src/q577/Main2.java
+++ src/q577/Main2.java
@@ -1,13 +1,20 @@
 package q577;
 
+import java.util.Scanner;
+
 public class Main2 {
 	private int max;
 	private int min;
-	
+	static int ab[] = new int [2]; 
 	
 	public static void main(String[] args) {
-
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int min = Math.max(a,b);
+		int max = Math.min(a,b);
+		
 	}
 
 }
